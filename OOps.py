23fed33f94e771ec270ecf1dde3f9af84430bdca
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2024-08-13 15:13:42 +0500

    inheritence

---
+++ OOps.py
@@ -0,0 +1,8 @@
+class Cars:
+    def __init__(self,brand,model):
+        self.brand = brand
+        self.model = model
+
+my_carr =  Cars("toytoa","prado")
+print(my_carr.model)
+print(my_carr.brand)
