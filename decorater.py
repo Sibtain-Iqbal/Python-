17e957a8d4ecfd1ecaddec71e3f26cf92d10b311
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2024-08-15 17:03:13 +0500

    lab

---
+++ decorater.py
@@ -0,0 +1,18 @@
+import time 
+
+def timer(func):
+    def wraper(*args,**kwargs):
+        start = time.time()
+        result = func(*args,**kwargs)
+        end = time.time()
+        print(f"{func.__name__} is run in {end-start}")
+        return result
+    return wraper 
+@timer
+def example_func(w):
+    time.sleep(w)
+example_func(2)
+
+def timer2():
+    print()
+timer()
